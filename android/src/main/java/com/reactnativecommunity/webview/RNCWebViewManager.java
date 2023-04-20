7542d388a03b3e44e11831917564aac2269f0b9f
sofiajacTE <126660997+sofiajacTE@users.noreply.github.com>
2023-04-20 16:00:06 +0200

    Android user agent fix

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -161,7 +161,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
 
   protected RNCWebChromeClient mWebChromeClient = null;
   protected boolean mAllowsFullscreenVideo = false;
-  protected @Nullable String mUserAgent = null;
+  protected @Nullable String mUserAgent = "teamengine-app-android/1.0 Chrome/104";
   protected @Nullable String mUserAgentWithApplicationName = null;
   protected @Nullable String mDownloadingMessage = null;
   protected @Nullable String mLackPermissionToDownloadMessage = null;
@@ -429,14 +429,15 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
   }
 
   protected void setUserAgentString(WebView view) {
-    if(mUserAgent != null) {
-      view.getSettings().setUserAgentString(mUserAgent);
-    } else if(mUserAgentWithApplicationName != null) {
-      view.getSettings().setUserAgentString(mUserAgentWithApplicationName);
-    } else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-      // handle unsets of `userAgent` prop as long as device is >= API 17
-      view.getSettings().setUserAgentString(WebSettings.getDefaultUserAgent(view.getContext()));
-    }
+    view.getSettings().setUserAgentString("teamengine-app-android/1.0 Chrome/104");
+    // if(mUserAgent != null) {
+    //   view.getSettings().setUserAgentString(mUserAgent);
+    // } else if(mUserAgentWithApplicationName != null) {
+    //   view.getSettings().setUserAgentString(mUserAgentWithApplicationName);
+    // } else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+    //   // handle unsets of `userAgent` prop as long as device is >= API 17
+    //   view.getSettings().setUserAgentString(WebSettings.getDefaultUserAgent(view.getContext()));
+    // }
   }
 
   @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
@@ -562,7 +563,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
             String key = iter.nextKey();
             if ("user-agent".equals(key.toLowerCase(Locale.ENGLISH))) {
               if (view.getSettings() != null) {
-                view.getSettings().setUserAgentString(headers.getString(key));
+                view.getSettings().setUserAgentString("teamengine-app-android/1.0 Chrome/104");
               }
             } else {
               headerMap.put(key, headers.getString(key));
@@ -901,6 +902,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
     protected RNCWebView.ProgressChangedFilter progressChangedFilter = null;
     protected @Nullable String ignoreErrFailedForThisURL = null;
     protected @Nullable BasicAuthCredential basicAuthCredential = null;
+    boolean userAgentBroken = false;
 
     public void setIgnoreErrFailedForThisURL(@Nullable String url) {
       ignoreErrFailedForThisURL = url;
@@ -927,6 +929,11 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
     public void onPageStarted(WebView webView, String url, Bitmap favicon) {
       super.onPageStarted(webView, url, favicon);
       mLastLoadFailed = false;
+      
+      if (userAgentBroken) {
+        webView.getSettings().setUserAgentString("teamengine-app-android/1.1 Chrome/104");
+        userAgentBroken = false;
+      }
 
       RNCWebView reactWebView = (RNCWebView) webView;
       reactWebView.callInjectedJavaScriptBeforeContentLoaded();
@@ -936,6 +943,15 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
         new TopLoadingStartEvent(
           webView.getId(),
           createWebViewEvent(webView, url)));
+    }
+    
+    @Override
+    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+      if (!request.getRequestHeaders().get("User-Agent").startsWith("teamengine")) {
+          System.out.println("User agent reverted");
+          userAgentBroken = true;
+      }
+      return null;
     }
 
     @Override
@@ -1259,6 +1275,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
       if (progressChangedFilter.isWaitingForCommandLoadUrl()) {
         return;
       }
+      webView.getSettings().setUserAgentString("teamengine-app-android/1.0 Chrome/104");
       WritableMap event = Arguments.createMap();
       event.putDouble("target", webView.getId());
       event.putString("title", webView.getTitle());
